a185dcde5462c478f703c2258e28bb26a9c3da64
Marko Kasović <marko.kasovic@igea.hr>
2017-11-28 13:37:54 +0100

    Add support for Message struct

--- component/src/main/ballerina/ballerina/azure/iotdevice/natives.bal
+++ component/src/main/ballerina/ballerina/azure/iotdevice/natives.bal
@@ -4,19 +4,27 @@ public struct ConnectionProperties {
 	string name;
 }
 
-@Description { value:"Azure IoT client connector."}
+public struct Message {
+    string messageId;
+    string correlationId;
+    string payload;
+    int expiryTime;
+    map properties;
+}
+
+@Description { value:"Azure IoT Device Client Connector."}
 @Param { value:"host: Host addresses of Azure IoT Hub" }
 @Param { value:"deviceId: Device ID" }
 @Param { value:"sharedAccessKey: Shared access key" }
-@Param { value:"options: Optional properties for Azure IoT connection" }
+@Param { value:"options: Optional properties for Azure IoT Hub connection" }
 public connector ClientConnector (string host, string deviceId, string sharedAccessKey, ConnectionProperties options) {
 
     map sharedMap = {};
 
     @Description {value:"The send action implementation which sends a message to Azure IoT Hub."}
-    native action send (json document, function() callbackFunction);
+    native action send (Message message, function(Message, string) callback);
 
-	@Description {value:"The close action implementation which closes the Azure IoT connection."}
+    @Description {value:"The close action implementation which closes the Azure IoT connection."}
     native action close ();
 
 }
--- component/src/main/java/hr/in2/ballerina/azure/iotdevice/actions/Send.java
+++ component/src/main/java/hr/in2/ballerina/azure/iotdevice/actions/Send.java
@@ -30,13 +30,16 @@ import org.ballerinalang.connector.api.ConnectorFuture;
 import org.ballerinalang.model.types.TypeKind;
 import org.ballerinalang.model.values.BConnector;
 import org.ballerinalang.model.values.BFunctionPointer;
-import org.ballerinalang.model.values.BJSON;
+import org.ballerinalang.model.values.BMap;
+import org.ballerinalang.model.values.BString;
+import org.ballerinalang.model.values.BStruct;
 import org.ballerinalang.model.values.BValue;
 import org.ballerinalang.natives.annotations.Argument;
 import org.ballerinalang.natives.annotations.BallerinaAction;
-import org.ballerinalang.natives.annotations.ReturnType;
 import org.ballerinalang.util.codegen.ProgramFile;
 import org.ballerinalang.util.program.BLangFunctions;
+
+import java.util.Iterator;
 
 /**
  * {@code Send} action to send a number of event messages to an Azure IoT Hub.
@@ -49,22 +52,21 @@ import org.ballerinalang.util.program.BLangFunctions;
             connectorName = Constants.CONNECTOR_NAME,
             args = {
                 @Argument(name = "c", type = TypeKind.CONNECTOR),
-                @Argument(name = "document", type = TypeKind.JSON)
-            },
-            returnType = { @ReturnType(type = TypeKind.JSON) }
+                @Argument(name = "message", type = TypeKind.STRUCT),
+                @Argument(name = "callback", type = TypeKind.FUNCTION)
+            }
          )
 public class Send extends AbstractIoTAction {
 
     @Override
     public ConnectorFuture execute(Context context) {
         BConnector bConnector = (BConnector) getRefArgument(context, 0);
-        BJSON document = (BJSON) getRefArgument(context, 1);
+        BStruct bMessage = (BStruct) getRefArgument(context, 1);
         BFunctionPointer pointer = (BFunctionPointer) getRefArgument(context, 2);
 
         AzureIoTDataSource dataSource = getDataSource(bConnector);
 
-        Message deviceMessage = new Message(document.getMessageAsString());
-        deviceMessage.setMessageId(java.util.UUID.randomUUID().toString());
+        Message deviceMessage = message(bMessage);
 
         dataSource.getDeviceClient().sendEventAsync(deviceMessage, new IotHubEventCallback() {
             @Override
@@ -72,13 +74,43 @@ public class Send extends AbstractIoTAction {
                 ProgramFile programFile = context.getProgramFile();
                 Context newContext = new WorkerContext(programFile, context);
 
-                BValue[] results = BLangFunctions.invokeFunction(programFile, pointer.value().getFunctionInfo(), null,
+                BValue[] args = new BValue[2];
+                args[0] = bMessage;
+                args[1] = new BString(responseStatus.name());
+
+                BValue[] results = BLangFunctions.invokeFunction(programFile, pointer.value().getFunctionInfo(), args,
                         newContext);
                 // TODO: handle invocation results
             }
-        }, deviceMessage);
+        }, bMessage);
 
         return getConnectorFuture();
+    }
+
+    protected Message message(BStruct bMessage) {
+        String messageId = bMessage.getStringField(0);
+        String correlationId = bMessage.getStringField(1);
+        String payload = bMessage.getStringField(2);
+        long expiryTime = bMessage.getIntField(0);
+        BMap<String, BValue> properties = (BMap<String, BValue>) bMessage.getRefField(0);
+
+        Message message = new Message(payload);
+        if (!messageId.equals("")) {
+            message.setMessageId(messageId);
+        }
+        if (!correlationId.equals("")) {
+            message.setCorrelationId(correlationId);
+        }
+        if (expiryTime > 0) {
+            message.setExpiryTime(bMessage.getIntField(0));
+        }
+        if (!properties.isEmpty()) {
+            for (Iterator<String> iterator = properties.keySet().iterator(); iterator.hasNext();) {
+                String key = iterator.next();
+                message.setProperty(key, properties.getValueAsString(key));
+            }
+        }
+        return message;
     }
 
 }
